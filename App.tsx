bdc890349789cbe90e579221f8ca40cc1832ab2d
Lisa-Shima <shimalisa2@gmail.com>
2025-05-07 09:48:40 +0200

    Committing - Added a simple count incrementer

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Count</Text>
+      <Text style={styles.counterText}>{count}</Text>
+
+      <Pressable
+      style={styles.button}
+      onPress={() => setCount(count + 1)}
+      >
+        <Text style={styles.buttonText}>Increment</Text>
+      </Pressable>
     </View>
   );
 }
@@ -17,4 +26,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontSize: 24,
+    marginBottom: 20
+  },
+  counterText: {
+    fontSize: 48,
+    marginVertical: 20
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 8,
+    backgroundColor: '#007AFF'
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18
+  }
 });
